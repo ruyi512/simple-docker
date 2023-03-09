257f6d108b127a5d46509fcd880679744ebd9cf4
wangruyi <wangruyi@example.com>
2023-03-09 12:08:52 +0800

    feat: web服务增加skywalking探针

--- web/workspace/main.py
+++ web/workspace/main.py
@@ -1,6 +1,16 @@
 from flask import Flask
 import sys
 import os
+from skywalking import agent, config
+
+config.init(agent_collector_backend_services='192.168.1.130:11800', agent_protocol='grpc',
+            agent_name='great-app-consumer-grpc',
+            agent_instance_name='py_demo',
+            agent_experimental_fork_support=True, agent_logging_level='DEBUG', agent_log_reporter_active=True,
+            agent_meter_reporter_active=True,
+            agent_profile_active=True)
+
+agent.start()
 
 app = Flask(__name__)
 
